ee0d32e26eb2e115a67f70fa2bb6f1d226fcbc3b
puria-radmard <pr450@cam.ac.uk>
2020-06-09 11:06:14 +0100

    packaged enc-dec, just need one json file

--- Encoder.py
+++ Encoder.py
@@ -3,90 +3,15 @@
 
 # In[248]:
 
-
-import fastai
 import torch
 import torch.nn as nn
 from torch.nn import init
-import numpy as np
-from nltk.tokenize import word_tokenize
-import nltk
-import json 
-import os
-import pickle
 
 # Loading all training examples, validation examples, and vocabulary
 
-def encoderDemo():
-
-    with open('quora_raw_train.json') as f:
-        training_examples = json.load(f)
-        
-    with open('quora_raw_val.json') as f:
-        val_examples = json.load(f)
-
-    with open('quora_data_prepro.json') as f:
-        vocab = json.load(f)['ix_to_word'] # Vocab = {1: 'the', 2: 'sauce' .....}
-    vocab_inv = {word:number for number,word in list(vocab.items())} # Inverted dictionary created          Vocab_inv = {'the':1, 'sauce':2, .....}
-
-    X_train_prelim = [training_examples[i]['question1'] for i in range(len(training_examples))] # Training examples before preprocessing
-    Y_train_prelim = [training_examples[i]['question'] for i in range(len(training_examples))]
-
-    X_val_prelim = [val_examples[i]['question1'] for i in range(len(val_examples))]
-    Y_val_prelim = [val_examples[i]['question'] for i in range(len(val_examples))]
-
-    X_train = []
-    Y_train = []
-
-    def load_glove_embeddings(filename="glove.6B.100d.txt"):
-        lines = open(filename).readlines()
-        print(len(lines))
-        embeddings = {}
-        a = 0
-        for line in lines:
-            a +=1
-            print(a)
-            word = line.split()[0]
-            embedding = list(map(float, line.split()[1:]))
-            if word in vocab.values():
-                embeddings[int(vocab_inv[word])] = embedding # This is creating a dictionary with indexes corresponding to the position of the particular word in the vocabulary
-                # embeddings = {1:embedding of 'the', 2: embedding of 'sauce'....}
-        return embeddings
-
-    embedded_words = load_glove_embeddings()
-
-    for i in range(len(X_train_prelim)): # This is basically tokenising each sentence, and converting it into a list of word embeddings. X corresponds to a training example and Y corresponds to its paraphrase
-        print(i)
-        sentence_x = X_train_prelim[i]
-        sentence_y = Y_train_prelim[i]
-        tokensx = word_tokenize(sentence_x)
-        tokensy = word_tokenize(sentence_y)
-        vectorx = []
-        vectory = []
-        for word in tokensx:
-            try:
-                vectorx.append(embedded_words[int(vocab_inv[word])])
-            except:
-                vectorx.append(-2*torch.rand(100,1)) # If the word is not in the embeddings given, then add a random vector (might have to do something about this :) ) (Potentially could write a spell checker to find misspellings)
-        for word in tokensy:
-            try:
-                vectorx.append(embedded_words[int(vocab_inv[word])])
-            except:
-                vectory.append(-2*torch.rand(100,1))
-
-        X_train.append(vectorx)
-        Y_train.append(vectory)
-
-
-    input_size = 100
-    hidden_size = 128
-    num_layers = 1
-    vocab_size = len(vocab)
-    dropout = 0.2
-    bidirectional = False
 
 class Encoder(nn.Module): # Encoder with a single layer LSTM 
-    def __init__(self,input_size,hidden_size,dropout,vocab_size,num_layers,bidirectional= False):
+    def __init__(self,input_size,hidden_size,dropout,vocab_size,num_layers, embedded_words, bidirectional= False):
         super(Encoder,self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
@@ -97,11 +22,14 @@ class Encoder(nn.Module): # Encoder with a single layer LSTM
         self.embedding = nn.Embedding(vocab_size,input_size,sparse=False) # Lookup table with word vectors and word index in vocab
         self.lstm_layer = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True,bidirectional = False) # LSTM moodule
         self.dropout = nn.Dropout(dropout)
+        self.embedded_words = embedded_words
         self.init_weights()
         
     def forward(self, x, prev_state):  # X corresponds to the index of the word in the vocabulary (so X can be 2 or 49 etc, and embedding(x) will give the word vector of the 2nd or 49th word in the vocabulary)
         embed = self.embedding(x) # Looking up embeddings
         output, curr_state = self.dropout(self.lstm_layer(embed, prev_state))
+
+        # Here curr_state is the tuple (hidden state, cell state)
         return output,curr_state
     
     def init_weights(self): # Initialising weights
@@ -109,13 +37,23 @@ class Encoder(nn.Module): # Encoder with a single layer LSTM
         init.uniform_(self.lstm_layer.weight_hh_l0, a=-0.01, b=0.01) # Values taken from the paper
         embedding_weights = torch.FloatTensor(self.vocab_size, self.input_size)
         init.uniform_(embedding_weights, a=-0.25, b=0.25) # Values taken from the paper
-        for k,v in embedded_words.items():
+        for k,v in self.embedded_words.items():
             embedding_weights[k,:] = torch.FloatTensor(v)
         embedding_weights[0] = torch.FloatTensor([0]*self.input_size)
         del self.embedding.weight # Deleting any pre-initialised embeddings in nn.embedding.weights and replacing it with our known word embeddings
         self.embedding.weight = nn.Parameter(embedding_weights)
 
-def trail_encoder():    
+
+
+def trial_encoder():    
+
+    input_size = 100
+    hidden_size = 128
+    num_layers = 1
+    vocab_size = len(vocab)
+    dropout = 0.2
+    bidirectional = False
+
     encoder = Encoder(input_size,hidden_size,dropout,vocab_size,num_layers,bidirectional)
 
     loss_fn = torch.nn.BCELoss()
